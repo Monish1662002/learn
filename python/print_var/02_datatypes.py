3ddd26869793667aa55ec203ccf334835ade69d9
Monish <monish1662002@gmail.com>
2025-08-28 13:10:06 +0530

    datatyoes

---
+++ python/print_var/02_datatypes.py
@@ -0,0 +1,60 @@
+#int
+a = 5
+b = -3
+print(a,b)
+# print(type(a,b))
+print(type(a))
+print(type(b))
+
+
+#float
+a = 5.0
+print(a)
+print(type(a))
+
+a = -0.4
+print(a)
+print(type(a))
+
+# d=0.0.5 #incorrect
+
+
+#bool
+a = True
+print(a)
+print(type(a))
+
+# a = false #incorrect should be in pascal case
+print(a)
+print(type(a))
+
+
+#string
+#single line String
+a = 'hello'
+c = "7"
+print(a,c)
+print(type(a))
+print(type(c))
+
+#multi line String
+#bli = '''hello '''   #incorrect
+#print(bli)
+#print(type(bli))
+a="""hello
+world"""
+b="'hello  Boys'"
+c='''hello
+world'''
+print(a)
+print(b)
+print(c)
+print(type(a))  
+print(type(b))
+print(type(c))
+
+#None
+a = None   #in python None is used instead of null(Pascal), null(Java,JavaScript)
+#a=null  #incorrect 
+print(a)
+print(type(a))
